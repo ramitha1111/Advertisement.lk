bc53c7cdc84788ef1e4647db56f4d954fc528260
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-03-28 13:07:37 +0530

    Ad password reset

--- Back-End/routes/authRoutes.js
+++ Back-End/routes/authRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const passport = require('passport');
 const { register, login, googleAuth } = require('../controllers/authController');
-const { sendOTP, verifyOTP } = require('../controllers/otpController');
+const { passwordResetOTP, verifyOTP } = require('../controllers/otpController');
 
 
 const router = express.Router();
 
 // Register user
 router.post('/register', register); // register method should be defined in authController.js
+
+//Send OTP
+router.post('/send-otp', passwordResetOTP); // sendOTP method should be defined in otpController.js
 
 // Verify OTP
 router.post('/verify-otp', verifyOTP); // verifyOTP method should be defined in authController.js
