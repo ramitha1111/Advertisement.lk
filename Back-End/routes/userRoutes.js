8061f2cac9c20856867e3e6b06640e04d3487bfc
danindu2024 <daninduransika@gmail.com>
2025-03-29 05:52:26 +0530

    added route to get user by ID

--- Back-End/routes/userRoutes.js
+++ Back-End/routes/userRoutes.js
@@ -3,6 +3,7 @@ const { updateUser } = require('../controllers/userController');
 const { authenticate } = require('../middlewares/authMiddleware');  // Assuming authMiddleware is used for JWT protection
 const { getUsers } = require('../controllers/userController');
 const { isAdmin } = require('../middlewares/roleMiddleware'); // Assuming you have an admin check
+const { getUserById } = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -14,5 +15,8 @@ router.delete('/delete', authenticate, deleteUser);
 
 // Get users - Only accessible to admins
 router.get('/', authenticate, isAdmin, getUsers); // Protect this route with JWT and admin verification
+
+// Get user by ID
+router.get('/:id', authenticate, getUserById);
 
 module.exports = router;
