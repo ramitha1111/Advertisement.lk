aca8f4cb3ecbd84e76b59d324ee6e82197308c3a
danindu2024 <daninduransika@gmail.com>
2025-03-29 05:46:46 +0530

    set routes to get users. Only admin has the privilege

--- Back-End/routes/userRoutes.js
+++ Back-End/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { updateUser } = require('../controllers/userController');
 const { authenticate } = require('../middlewares/authMiddleware');  // Assuming authMiddleware is used for JWT protection
+const { getUsers } = require('../controllers/userController');
+const { isAdmin } = require('../middlewares/roleMiddleware'); // Assuming you have an admin check
 
 const router = express.Router();
 
@@ -9,5 +11,8 @@ router.put('/update', authenticate, updateUser); // Protect this route with JWT
 
 // Delete user
 router.delete('/delete', authenticate, deleteUser);
+
+// Get users - Only accessible to admins
+router.get('/', authenticate, isAdmin, getUsers); // Protect this route with JWT and admin verification
 
 module.exports = router;
