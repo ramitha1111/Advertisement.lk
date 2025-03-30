c21ac0b26d3152f5457b8353e63dc96904474a10
Sandaru Mihiran <mihiranwee17@gmail.com>
2025-03-31 00:08:30 +0530

    Fixed missing imports
    defined, but didn’t import getAdvertisementByCategory from the controller.

--- Back-End/routes/AdvertisementRoutes.js
+++ Back-End/routes/AdvertisementRoutes.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const { createAdvertisement, getAdvertisements, getAdvertisement, updateAdvertisement, deleteAdvertisement,
-    getAdvertisementsByAdvertisementId
+    getAdvertisementsByAdvertisementId,getAdvertisementByCategory,getAdvertisementsBySearch,getCompareAds,addToCompareList,removeFromCompareList
 } = require('../controllers/advertisementController');
 const routes=express.Router();
 
@@ -10,9 +10,12 @@ routes.get('/', getAdvertisements); // Get all advertisements
 routes.get('/:id', getAdvertisement); // Get a single advertisement by ID
 routes.put('/:id', updateAdvertisement); // Update advertisement
 routes.delete('/:id', deleteAdvertisement); // Delete advertisement
-routes.get('/:categories,getAdvertisementByCategory');//Get advertisements by category
+routes.get('/:categories',getAdvertisementByCategory);//Get advertisements by category , make an adjustment by Mihiran on this line 
 routes.get('/:advertisementId',getAdvertisementsByAdvertisementId);//Get advertisements by id
 routes.get('/:search',getAdvertisementsBySearch);//Get advertisements by search
+routes.post('/compare/:id',addToCompareList); // Add ad to compare list
+routes.delete('/compare/:id',removeFromCompareList);//remove ad from compare list 
+routes.get('/compare',getCompareAds);// Get compared Ads
 
 
 module.exports=routes;
