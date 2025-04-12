f8bbc03cb6b326b501d033a771cfe9fd87739778
Ishara Dhanushan <109136246+isharadh2002@users.noreply.github.com>
2025-04-12 14:16:35 +0530

    Creating Order Functions

---
+++ Back-End/routes/orderRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const {
+    getUserOrders,
+    getOrderById,
+    getAllOrders
+} = require('../controllers/orderController')
+const {isAuthenticated} = require('../middlewares/authMiddleware');
+
+const router = express.Router();
+
+// User routes
+router.get('/my-orders', isAuthenticated, getUserOrders);
+router.get('/:id', isAuthenticated, getOrderById);
+
+// Admin route
+router.get('/all', isAuthenticated, getAllOrders);
+
+module.exports = router;
