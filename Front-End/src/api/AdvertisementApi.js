5807c339eff5127dc7d45227c85d8e13c6ee1c52
Ramitha Iddamalgoda <141983882+Rami2212@users.noreply.github.com>
2025-04-21 16:45:32 +0530

    Update AdvertisementApi.js

--- Front-End/src/api/AdvertisementApi.js
+++ Front-End/src/api/AdvertisementApi.js
@@ -1,119 +1,83 @@
 import api from "../axios.js";
-/*
-export const deleteUser = async (userId, token) => {
-    const response = await api.delete(`/users/${userId}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
-};*/
 
 
-//Get all advertisements
-export const getAllAdvertisements = async (token) => {
-    const response = await api.get("/advertisements/", {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
-};
-//Get advertisements by userId
-export const getAdvertisementsByUserId = async (token) => {
-    const response = await api.get("/advertisements/user/", {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
-}
-
-
-
-
-//Update Advertisement
-export const updateAdvertisement = async (advertisementId, advertisementData, token) => {
-    const response = await api.put(`/advertisements/${advertisementId}`, advertisementData, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
+// Get all advertisements
+export const getAllAdvertisements = async () => {
+  const response = await api.get("/advertisements");
+  return response.data;
 };
 
-//Delete Advertisement
-export const deleteAdvertisement = async (advertisementId, token) => {
-    const response = await api.delete(`/advertisements/info/${advertisementId}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
+// Get advertisements by logged-in user
+export const getAdvertisementsByUser = async (token) => {
+  const response = await api.get("/advertisements/user", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
 };
-//Get advertisements by category
-export const getAdvertisementsByCategory = async (categoryId, token) => {
-    const response = await api.get(`/advertisements/categories/${categoryId}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
-};
-//Get advertisements by AdvertisementId
-export const getAdvertisementsByAdvertisementId = async (advertisementId, token) => {
-    const response = await api.get(`/advertisements/${advertisementId}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
-};
-//Get advertisements by searching
-export const getAdvertisementsBySearching = async (search, token) => {
-    const response = await api.get(`/advertisements/search/${search}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
-};
-//Get renewable advertisements
-export const getRenewableAds = async (token) => {
-    const response = await api.get("/advertisements/renewable-ads", {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
 
+// Get single advertisement by ID
+export const getAdvertisementById = async (advertisementId) => {
+  const response = await api.get(`/advertisements/${advertisementId}`);
+  return response.data;
 };
-//create advertisement
+
+// Create new advertisement (with image)
 export const createAdvertisement = async (advertisementData, token) => {
-    const response = await api.post("/advertisements/", advertisementData, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'multipart/form-data', // Important for file uploads
-        },
-    });
-
-
-
-
-    return response.data;
+  const response = await api.post("/advertisements", advertisementData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "multipart/form-data", // Important for image uploads
+    },
+  });
+  return response.data;
 };
 
-
-
-//get advertisements by filtering
-export const getAdvertisementsByFiltering = async (category, location, priceRange, token) => {
-    const response = await api.get(`/advertisements/filter/${category}/${location}/${priceRange}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.data;
-
-
+// Update advertisement
+export const updateAdvertisement = async (advertisementId, advertisementData, token) => {
+  const response = await api.put(`/advertisements/${advertisementId}`, advertisementData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
 };
 
+// Delete advertisement
+export const deleteAdvertisement = async (advertisementId, token) => {
+  const response = await api.delete(`/advertisements/${advertisementId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+// Get advertisements by category ID
+export const getAdvertisementsByCategory = async (categoryId) => {
+  const response = await api.get(`/advertisements/categories/${categoryId}`);
+  return response.data;
+};
+
+// Search advertisements (by keyword)
+export const getAdvertisementsBySearch = async (searchKeyword) => {
+  const response = await api.get(`/advertisements/search/${searchKeyword}`);
+  return response.data;
+};
+
+// Filter advertisements by category, location, and price range
+export const getAdvertisementsByFilter = async (category, location, priceRange) => {
+  const response = await api.get(`/advertisements/filter/${category}/${location}/${priceRange}`);
+  return response.data;
+};
+
+// Get advertisements eligible for renewal
+export const getRenewableAdvertisements = async (token) => {
+  const response = await api.get("/advertisements/renewable-ads", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
