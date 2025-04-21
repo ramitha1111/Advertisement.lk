4e542734e49671d30939143cdd78308a60d6009d
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-04-21 16:21:43 +0530

    add advertisements

--- Front-End/src/pages/user/EditeAdvertisement.jsx
+++ Front-End/src/pages/user/EditeAdvertisement.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
-import { setError, setLoading } from "../../store/userDashboardSlice.js";
 
 const EditAdvertisement = ({ ad, onUpdate }) => {
   const { id } = useParams();
