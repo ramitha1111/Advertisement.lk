42890b6dd4432ac88ce34d38c42a6bcebae05df3
Ramitha Iddamalgoda <141983882+Rami2212@users.noreply.github.com>
2025-04-23 19:33:56 +0530

    Update advertisementApi.js

--- Front-End/src/api/advertisementApi.js
+++ Front-End/src/api/advertisementApi.js
@@ -9,7 +9,6 @@ export const getAllAdvertisements = async () => {
 
 // Get advertisements by logged-in user
 export const getAdvertisementsByUser = async (userId, token) => {
-  console.log("id ", userId, " token ", token)
   const response = await api.get(`/advertisements/user/${userId}`, {
     headers: {
       Authorization: `Bearer ${token}`,
