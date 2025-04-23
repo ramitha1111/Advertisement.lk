db503e7f00ce7ce0754b65bb9f34a0cd81294204
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-04-23 14:50:01 +0530

    fix compares

---
+++ Front-End/src/api/compareApi.js
@@ -0,0 +1,29 @@
+// userApi.js
+import api from '../axios';
+
+export const getAllCompares = async (token) => {
+  const response = await api.get(`/compare`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response;
+};
+
+export const createCompare = async (advertisementId, token) => {
+  const response = await api.put(`/compare/${advertisementId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+export const deleteCompare = async (advertisementId, token) => {
+  const response = await api.delete(`/compare/${advertisementId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response;
+};
